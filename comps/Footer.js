76b337f45cd37c1fc2d2f56cbf959c2836ac5f2c
SMVahidhosseini <moein.vahidhosseini@gmail.com>
2022-05-23 23:51:33 +0430

    Corrected footer

--- comps/Footer.js
+++ comps/Footer.js
@@ -1,7 +1,34 @@
 const Footer = () => {
 	return (
 		<div className="footer">
-			This is Footer
+			<div className="footersubdiv">
+				Designed and developed by <span className="footerName">Seyed Mohammad Vahidhosseini</span>
+			</div>
+			<div className="footersubdiv">
+				Ph.D. in Mechanical Engineering - Energy Conversion Division,
+			</div>
+			<div className="footersubdiv">
+				Mechanical Engineering Department, Ferdowsi University of Mashhad, Mashhad, Iran
+			</div>
+			<div className="footersubdiv">
+				Front-end web developer; React, Next, Svelte
+			</div>
+			<div className="footersubdiv">
+				E-mail: <span className="footerName">moein.vahidhosseini@gmail.com</span>
+			</div>
+			<div className="footersubdiv">
+				<div className="footersocial">
+					<a href='https://scholar.google.com/citations?user=roHaKVcAAAAJ&hl=en' target="_blank" rel="noreferrer">
+						<img src="/icons/scholar.svg" /> 
+					</a>
+					<a href='https://www.linkedin.com/in/seyed-mohammad-vahidhosseini-47468a151?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BEFQGpDNBQLOn5e%2BxjN9g6w%3D%3D' target="_blank" rel="noreferrer">
+						<img src="/icons/linkedin.svg" /> 
+					</a>
+					<a href='https://www.researchgate.net/profile/Seyed-Mohammad-Vahidhosseini' target="_blank" rel="noreferrer">
+						<img src="/icons/researchgate.svg" width="42vw" height="46vw"/> 
+					</a>
+				</div>
+			</div>
 		</div>
 	);
 }

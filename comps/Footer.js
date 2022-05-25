db4de0fde48c4a301b480b6d27a97ef1b9a2c809
SMVahidhosseini <moein.vahidhosseini@gmail.com>
2022-05-25 10:54:49 +0430

    github added

--- comps/Footer.js
+++ comps/Footer.js
@@ -11,7 +11,7 @@ const Footer = () => {
 				Mechanical Engineering Department, Ferdowsi University of Mashhad, Mashhad, Iran
 			</div>
 			<div className="footersubdiv">
-				Front-end web developer; React, Next, Svelte
+				Front-end web developer; React, React Native, Next, Svelte
 			</div>
 			<div className="footersubdiv">
 				E-mail: <span className="footerName">moein.vahidhosseini@gmail.com</span>
@@ -19,13 +19,16 @@ const Footer = () => {
 			<div className="footersubdiv">
 				<div className="footersocial">
 					<a href='https://scholar.google.com/citations?user=roHaKVcAAAAJ&hl=en' target="_blank" rel="noreferrer">
-						<img src="/icons/scholar.svg" /> 
+						<img src="/icons/scholar.svg" />
 					</a>
 					<a href='https://www.linkedin.com/in/seyed-mohammad-vahidhosseini-47468a151?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BEFQGpDNBQLOn5e%2BxjN9g6w%3D%3D' target="_blank" rel="noreferrer">
-						<img src="/icons/linkedin.svg" /> 
+						<img src="/icons/linkedin.svg" />
 					</a>
 					<a href='https://www.researchgate.net/profile/Seyed-Mohammad-Vahidhosseini' target="_blank" rel="noreferrer">
-						<img src="/icons/researchgate.svg" width="42vw" height="46vw"/> 
+						<img src="/icons/researchgate.svg" width="42vw" height="46vw"/>
+					</a>
+					<a href='https://github.com/SMVahidhosseini' target="_blank" rel="noreferrer">
+						<img src="/icons/github.svg" width="50vw" height="50vw"/>
 					</a>
 				</div>
 			</div>
